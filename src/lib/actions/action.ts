d58a0dcbd846d6ed1943ddaba6d4e86c753c6522
bckspce <49206472+pwamoreno@users.noreply.github.com>
2024-12-14 10:36:22 +0100

    actions moodification

--- src/lib/actions/action.ts
+++ src/lib/actions/action.ts
@@ -13,26 +13,36 @@ export const getCollectionDetails = async(collectionId: string) => {
 }
 
 export const getProducts = async() => {
-    const products = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`)
+    const products = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`,{
+        cache: "no-store"
+    })
     return await products.json()
 }
 
 export const getProductDetails =  async(productId: string) => {
-    const product = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`)
+    const product = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`,{
+        cache: "no-store"
+    })
     return await product.json()
 }
 
 export const getSearchedProducts = async(query: string) => {
-    const searchedProducts = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/search/${query}`)
+    const searchedProducts = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/search/${query}`,{
+        cache: "no-store"
+    })
     return await searchedProducts.json()
 }
 
 export const getOrders = async(customerId: string) => {
-    const orders = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/orders/customers/${customerId}`)
+    const orders = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/orders/customers/${customerId}`,{
+        cache: "no-store"
+    })
     return await orders.json()
 }
 
 export const getRelatedProducts = async(productId: string) => {
-    const relatedProducts = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}/related`)
+    const relatedProducts = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}/related`,{
+        cache: "no-store"
+    })
     return await relatedProducts.json()
 }
